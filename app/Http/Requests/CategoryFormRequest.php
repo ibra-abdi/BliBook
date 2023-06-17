7c12aa979a00e5d762cb1ff8b7b69ec168231b4b
ibra-abdi <96337359+ibra-abdi@users.noreply.github.com>
2023-06-17 21:50:24 +0700

    Add Category

---
+++ app/Http/Requests/CategoryFormRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryFormRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return view('admin.category.index');
+    }
+
+
+    //   @return array<string, mixed>
+
+    public function rules()
+    {
+        return[
+            'name' => [
+                'required',
+                'string'
+            ],
+            'slug' => [
+                'required',
+                'string'
+            ],
+            'description' => [
+                'required',
+            ],
+            'image' => [
+                'nullable',
+                'mimes: jpg,jpeg,png'
+            ],
+            'meta_title' => [
+                'required',
+                'string'
+            ],
+            'meta_keyword' => [
+                'required',
+                'string'
+            ],
+            'meta_description' => [
+                'required',
+                'string'
+            ],
+        ];
+    }
+}
